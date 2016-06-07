cc9f52396f3cd3f3720584a9cb67f37981a435dc
Paprotsky <e.paprotsky@itransition.corp>
2016-06-07 14:49:59 +0300

    Update bubbleSortings

--- sortings/bubbleSort.js
+++ sortings/bubbleSort.js
@@ -2,7 +2,7 @@
 
 function bubbleSort(array) {
     var arrayLength = array.length;
-    for (var outer = array; outer >= 2; --outer) {
+    for (var outer = arrayLength; outer >= 2; --outer) {
         for (var inner = 0; inner <= outer - 1; ++inner) {
             if (array[inner] > array[inner + 1]) {
                 swap(array, inner, inner + 1);
