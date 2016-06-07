a71470ee1c606cd47bbd051cffe455b1a2b99b2e
Paprotsky <e.paprotsky@itransition.corp>
2016-06-07 14:47:48 +0300

    Update bubble Sort

--- sortings/bubbleSort.js
+++ sortings/bubbleSort.js
@@ -4,8 +4,9 @@ function bubbleSort(array) {
     var arrayLength = array.length;
     for (var outer = array; outer >= 2; --outer) {
         for (var inner = 0; inner <= outer - 1; ++inner) {
-            if (dataArray[inner] > dataArray[inner + 1]) {
-                swap()
+            if (array[inner] > array[inner + 1]) {
+                swap(array, inner, inner + 1);
             }
         }
     }
+}
